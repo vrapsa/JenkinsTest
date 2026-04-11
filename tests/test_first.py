40dded4505010998a93194acb9fb97f3b5c734e0
vrapsa <vrapsa@yandex.ru>
2026-04-11 13:55:26 +0300

    init commit

---
+++ tests/test_first.py
@@ -0,0 +1,13 @@
+
+
+
+class TestFirst:
+
+    def test_first(self):
+        pass
+
+    def test_firstly(self):
+        pass
+
+    def test_first_one(self):
+        pass
---
+++ tests/test_service.py
@@ -0,0 +1,11 @@
+
+class TestService:
+
+    def test_first_service(self):
+        pass
+
+    def test_firstly_service(self):
+        pass
+
+    def test_first_one_service(self):
+        pass
